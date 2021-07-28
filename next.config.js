b0fab76d0bf4268e39f06c4257f21cdbabd3da61
Matt Levine <matthewklevine@gmail.com>
2021-07-28 15:59:09 -0700

    Adding 404.js

--- next.config.js
+++ next.config.js
@@ -48,7 +48,7 @@ module.exports = withTM(
       env: {
         rootpath: typeof process.env.MURA_ROOTPATH != 'undefined' ? process.env.MURA_ROOTPATH : 'http://localhost:8888',
         siteid: process.env.MURA_SITEID || ['default'],
-        editroute: '',
+        editroute: '/edit',
         sitename: typeof process.env.MURA_SITENAME != 'undefined' ? process.env.MURA_SITENAME :'Example Site',
         siteidinurls:  handleBooleanProperty(process.env.MURA_SITEIDINURLS),
         codeblocks: handleBooleanProperty(process.env.MURA_CODEBLOCKS),
