dc3893182078ce1791b9becd0bfef486cc63534b
Matt Levine <matthewklevine@gmail.com>
2021-08-10 12:47:50 -0700

    Working on template

--- src/pages/[...page].js
+++ src/pages/[...page].js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { setMuraConfig, MainLayout, MuraJSRefPlaceholder, getMuraProps, getRootPath, getMuraPaths, getSiteName } from '@murasoftware/next-core';
 import ErrorPage from 'next/error';
 import Body from '../components/Body';
 import muraConfig from 'mura.config';
+import Head from '@components/Head';
 
 export async function getStaticPaths() {
   setMuraConfig(muraConfig);
@@ -62,46 +62,13 @@ export default function Page(props) {
 
     return (
       <MainLayout {...props} route={`/${router.query.page}`}>  
-        <Head>
-          {/* I wanted to add a "MuraMetaTags" component here but doesn't seem possible inside the <Head> component -- see metaTags branch */}
-          <title>{content.htmltitle} - {getSiteName()}</title>
-          <meta name="description" content={content.metadesc} />
-
-          <meta property="og:site_name" content={getSiteName()} />
-          <meta property="og:title" content={content.htmltitle} />
-          <meta property="og:description" content={content.metadesc} />
-          {content.images && content.images.large &&
-            <meta property="og:image" content={content.images.large} />
-          }
-          <meta property="og:type" content="website" />
-          
-          {content.canonicalurl != '' &&
-            <link rel="canonical" href={content.canonicalurl} />
-          }
-
-          {content.canonicalurl == '' &&
-            <link rel="canonical" href={`${getRootPath()}/${content.filename}`} />
-          }
-
-          <link
-            href={`${getRootPath()}/core/modules/v1/core_assets/css/mura.10.min.css`}
-            rel="stylesheet"
-            key="min"
+         <Head            
+            content={content}
+            getSiteName={getSiteName}
+            MuraJSRefPlaceholder={MuraJSRefPlaceholder}
+            getRootPath={getRootPath}
+            codeblocks={props.codeblocks}
           />
-          <link
-            href={`${getRootPath()}/core/modules/v1/core_assets/css/mura.10.skin.css`}
-            rel="stylesheet"
-            key="skin"
-          />
-          {/* favicon */}
-          <link rel="icon" href="/ico/favicon.ico" type="image/x-icon" />
-          <link rel="shortcut icon" href="/ico/favicon.ico" type="image/x-icon" />
-          <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/ico/apple-touch-icon-144-precomposed.png" />
-          <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/ico/apple-touch-icon-114-precomposed.png" />
-          <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/ico/apple-touch-icon-72-precomposed.png" />
-          <link rel="apple-touch-icon-precomposed" href="/ico/apple-touch-icon-57-precomposed.png" />
-          <script dangerouslySetInnerHTML={{__html:MuraJSRefPlaceholder}}/>
-        </Head>
         <div dangerouslySetInnerHTML={{__html:props.codeblocks.header}}/>
         <div dangerouslySetInnerHTML={{__html:props.codeblocks.bodystart}}/>
         <Body
